4bc43f554762db81cf38493e493088556eb8fd55
DeonDp <deongohard@hotmail.com>
2017-02-11 12:57:16 -0800

    lab3exercise1
    send email script

---
+++ sendemail.py
@@ -0,0 +1,25 @@
+import smtplib 
+from_addr = 'noednikretep@gmail.com' 
+to_addr = 'deongohard@hotmail.com' 
+from_name ='Noed Nikretep'
+to_name ='Deon Peterkin'
+subject ='INFO3180 mail'
+msg= 'I am king'
+username ='noednikretep@gmail.com'
+password ='aug2010deon'
+message = """From: {} <{}> 
+To: {} <{}> 
+Subject: {} 
+{} 
+""" 
+message_to_send = message.format(from_name, from_addr, to_name, 
+to_addr, subject, msg) 
+# Credentials (if needed) 
+username = '' 
+password = '' 
+# The actual mail send 
+server = smtplib.SMTP('smtp.gmail.com:587') 
+server.starttls() 
+server.login(username, password) 
+server.sendmail(from_addr, to_addr, message_to_send) 
+server.quit()
